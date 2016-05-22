331ca38628375f12db9771d1cc59943c57ff259e
Stephen Printup <stephen.printup@gmail.com>
2016-05-22 08:12:23 -0600

    User accounts, authentication and permissions 🏋🏿

--- client/imports/party-details/party-details.ts
+++ client/imports/party-details/party-details.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouteParams, RouterLink } from '@angular/router-deprecated';
 import {Parties} from '../../../collections/parties.ts';
+import { Meteor } from 'meteor/meteor';
+import { RequireUser } from 'angular2-meteor-accounts-ui';
 
 @Component({
   selector: 'party-details',
   templateUrl: '/client/imports/party-details/party-details.html',
   directives: [RouterLink]
 })
+@RequireUser()
 export class PartyDetails {
   party: Party;
   constructor(params: RouteParams) {
@@ -15,12 +18,16 @@ export class PartyDetails {
   }
 
   saveParty(party) {
-    Parties.update(party._id, {
-      $set: {
-        name: party.name,
-        description: party.description,
-        location: party.location
-      }
-    });
+    if (Meteor.userId()) {
+      Parties.update(party._id, {
+        $set: {
+          name: party.name,
+          description: party.description,
+          location: party.location
+        }
+      });
+    } else {
+      alert('Please log in to change this party');
+    }
   }
 }
